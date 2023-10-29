e320c78478e9179695a22d8b5af3a4386e6902ce
justice-udehegbunam <justicefavour491@gmail.com>
2023-10-29 23:34:53 +0100

    Nav-bar ANd hero work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import Hero from "./components/Hero";
+import CustomerReviews from "./components/CustomerReviews";
+import Footer from "./components/Footer";
+import PopularProducts from "./components/PopularProducts";
+import Services from "./components/Services";
+import SpecialOffer from "./components/SpecialOffer";
+import Subscribe from "./components/Subscribe";
+import SuperQuality from "./components/SuperQuality";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="padding bg-pale-blue">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="padding-x padding-t bg-black pb-8">Footer</section>
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t bg-black pb-8">
+      <Footer />
+    </section>
   </main>
 );
 export default App;
