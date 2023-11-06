0a3646159762ce7e4c0430529cbd1ad388dc52c2
Justice-Udehegbunam <justicefavour491@gmail.com>
2023-11-06 21:49:54 +0100

    Subscribe and footer and final project completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
@@ -14,7 +15,7 @@ const Button = ({
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red text-white border-coral-red"
-      } rounded-full`}
+      } rounded-full ${fullWidth && "w-full"}"}`}
     >
       {label}
 
